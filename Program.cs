e48b93cf586c80151493265ff75e94d297dcade8
insomnyawolf <insomnyawolf@gmail.com>
2022-03-20 16:44:33 +0100

    simplified code while mantaining behaviour

--- Program.cs
+++ Program.cs
@@ -4,43 +4,37 @@
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 2)
             {
-                Console.WriteLine();
-                Console.WriteLine(" " + AppDomain.CurrentDomain.FriendlyName + " <input bank> <output fsb>");
+                PrintHelp();
+                return;
             }
-            else if (args.Length == 1)
-            {
-                Console.WriteLine();
-                Console.WriteLine(" " + AppDomain.CurrentDomain.FriendlyName + " <input bank> <output fsb>");
-            }
-            else if (args.Length == 2)
-            {
-                byte[] infile = File.ReadAllBytes(args[0]);
 
-                for (int i = 0; i < infile.Length; i++)
+            byte[] infile = File.ReadAllBytes(args[0]);
+
+            for (int i = 0; i < infile.Length; i++)
+            {
+                bool found = false;
+                string buffer = infile[i].ToString() + infile[i + 1].ToString() + infile[i + 2].ToString() + infile[i + 3].ToString();
+                if (buffer == "70836653")
                 {
-                    bool found = false;
-                    string buffer = infile[i].ToString() + infile[i + 1].ToString() + infile[i + 2].ToString() + infile[i + 3].ToString();
-                    if (buffer == "70836653")
-                    {
-                        int newlen = infile.Length - i;
-                        byte[] outfile = new byte[newlen];
-                        Array.Copy(infile, i, outfile, 0, newlen);
-                        File.WriteAllBytes(args[1], outfile);
-                        found = true;
-                    }
-                    if (found == true)
-                    {
-                        break;
-                    }
+                    int newlen = infile.Length - i;
+                    byte[] outfile = new byte[newlen];
+                    Array.Copy(infile, i, outfile, 0, newlen);
+                    File.WriteAllBytes(args[1], outfile);
+                    found = true;
+                }
+                if (found == true)
+                {
+                    break;
                 }
             }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine(" " + AppDomain.CurrentDomain.FriendlyName + " <input bank> <output fsb>");
-            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine();
+            Console.WriteLine(" " + AppDomain.CurrentDomain.FriendlyName + " <input bank> <output fsb>");
         }
     }
 }
